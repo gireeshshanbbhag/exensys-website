63781f8580dacc2d2e33a6d17dd331ee9b920561
gireeshshanbbhag <gireeshshanbbhag@gmail.com>
2026-09-08 16:37:42 +0530

    Add src/components/providers/theme.tsx

---
+++ src/components/providers/theme.tsx
@@ -0,0 +1,18 @@
+import {
+  ThemeProvider as NextThemeProvider,
+  type ThemeProviderProps,
+} from "next-themes";
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  return (
+    <NextThemeProvider
+      attribute="class"
+      defaultTheme="light"
+      enableSystem={false}
+      forcedTheme="light"
+      {...props}
+    >
+      {children}
+    </NextThemeProvider>
+  );
+}
