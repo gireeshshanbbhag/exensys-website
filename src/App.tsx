68381cfda37a76fc204afffab3509d6ffcbc8b8f
gireeshshanbbhag <gireeshshanbbhag@gmail.com>
2026-09-08 16:36:14 +0530

    Add src/App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { DefaultProviders } from "./components/providers/default.tsx";
+import AuthCallback from "./pages/auth/Callback.tsx";
+import Layout from "./components/layout.tsx";
+import Index from "./pages/Index.tsx";
+import NotFound from "./pages/NotFound.tsx";
+
+export default function App() {
+  return (
+    <DefaultProviders>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/auth/callback" element={<AuthCallback />} />
+          <Route element={<Layout />}>
+            <Route path="/" element={<Index />} />
+          </Route>
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </DefaultProviders>
+  );
+}
